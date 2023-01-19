827bb5311449097224dc06234cb48e27dccb5609
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-01-19 22:16:40 +0100

    feat: add winner page template

--- src/ts/templates/winnersView.ts
+++ src/ts/templates/winnersView.ts
@@ -1,9 +1,39 @@
 import { StoreI } from '../interfaces/storeI'
 
-export const winnersView = (store: StoreI) => {
+export const winnersView = ({ winnersCount, winnersPage, sortBy, sortOrder, winners }: StoreI) => {
     return `<div id="winners-view" >
+                <h1>Winners (${winnersCount})</h1>
+                <p>Page #${winnersPage}</p>
 
-                {renderWinners(stor)}
-
+                <table class='table' cellspacing="0" border="0" cellpadding="0">
+                    <thead>
+                        <th>Number</th>
+                        <th>Car name</th>
+                        <th class="table-button table-wins
+                            ${sortBy === 'wins' ? sortOrder : ''}"
+                            id="sort-by-wins">
+                            Wins
+                        </th>
+                        <th class="table-button table-time
+                            ${sortBy === 'time' ? sortOrder : ''}"
+                            id="sort-by-time">
+                            Best time
+                        </th>
+                    </thead>
+                    <tbody>
+                        ${winners
+                            .map(
+                                ({ car, wins, time }, i) => `
+                            <tr>
+                                <td>${i + 1}</td>
+                                <td>${car.name}</td>
+                                <td>${wins}</td>
+                                <td>${time}</td>
+                            </tr>
+                        `
+                            )
+                            .join('')}
+                    </tbody>
+                </table>
             </div>`
 }
